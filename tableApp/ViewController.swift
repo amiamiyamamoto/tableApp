46f87e454ca4a8da764f89ed4955f01dccf8bf61
AMI <a_yamamoto@innovation.co.jp>
2017-12-06 12:48:51 +0900

    git管理のため

--- tableApp/ViewController.swift
+++ tableApp/ViewController.swift
@@ -8,7 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITabBarDelegate {
+    
+    let members:[String] = ["近藤", "遠山", "山本"]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +21,37 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return members.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        
+        
+        //storyBoadで指定したidentify　todoCell識別子を利用して再利用可能なセルを取得する
+        //let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        //行番号に合ったToDoのタイトルを取得
+//        let title = todoList[indexPath.row]
+        let member = members[indexPath.row]
+        
+        //CellのラベルにToDoのタイトルをセット
+//        cell.textLabel?.text = todoTitle
+        cell.textLabel?.text = member
+        return cell
+
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 
